d85afcf001bc5248835edad275ed03c436c65b01
Joel Morel <Joel@JoelMorel.com>
2020-05-22 18:35:07 -0400

    added about,gallery,contact pages. added transitions animations between pages.

--- gsap/js/app.js
+++ gsap/js/app.js
@@ -1,142 +1,202 @@
-let timeline = gsap.timeline();
+const initPage = () => {
+  let timeline = gsap.timeline();
+  timeline
+    .fromTo(
+      ".info-section h1",
+      {
+        x: 0,
+        y: 0,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1,
+      }
+    )
+    .fromTo(
+      ".info-section h4",
+      {
+        x: -200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1,
+      }
+    )
 
-timeline
-  .fromTo(
-    ".info-section h1",
-    {
-      x: 0,
-      y: 0,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      y: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1,
-    }
-  )
-  .fromTo(
-    ".info-section h4",
-    {
-      x: -200,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1,
-    }
-  )
+    .fromTo(
+      ".logo",
+      {
+        x: -200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.5,
+      }
+    )
+    .fromTo(
+      ".menu",
+      {
+        x: 200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.5,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".back",
+      {
+        x: -500,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".front",
+      {
+        x: 0,
+        opacity: 0,
+      },
+      {
+        x: 275,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".shape1",
+      {
+        scale: 0.1,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 1,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".shape2",
+      {
+        scale: 0.1,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 0.4,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".shape3",
+      {
+        scale: 0,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 0.4,
+        delay: 0.5,
+        duration: 1.3,
+      },
+      ">-1.8"
+    )
+    .fromTo(
+      ".call-actions",
+      {
+        scale: 0,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 1,
+        //delay: 0.5,
+        duration: 1,
+      }
+    );
+};
 
-  .fromTo(
-    ".logo",
+const delay = (n) => {
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    });
+  });
+};
+
+const loadingLeave = () => {
+  let timeline = gsap.timeline();
+
+  timeline.fromTo(
+    "loading-bg",
     {
-      x: -200,
-      opacity: 0,
+      y: "100%",
     },
     {
-      x: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.5,
-    }
-  )
-  .fromTo(
-    ".menu",
-    {
-      x: 200,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.5,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".girl",
-    {
-      x: -500,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".boy",
-    {
-      x: 0,
-      opacity: 0,
-    },
-    {
-      x: 275,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".shape1",
-    {
-      scale: 0.1,
-      opacity: 0,
-    },
-    {
-      scale: 1,
-      opacity: 1,
-      delay: 0.5,
-      duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".shape2",
-    {
-      scale: 0.1,
-      opacity: 0,
-    },
-    {
-      scale: 1,
-      opacity: 0.4,
-      delay: 0.5,
-      duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".shape3",
-    {
-      scale: 0,
-      opacity: 0,
-    },
-    {
-      scale: 1,
-      opacity: 0.4,
-      delay: 0.5,
-      duration: 1.3,
-    },
-    ">-1.8"
-  )
-  .fromTo(
-    ".call-actions",
-    {
-      scale: 0,
-      opacity: 0,
-    },
-    {
-      scale: 1,
-      opacity: 1,
-      //delay: 0.5,
-      duration: 1,
+      y: 0,
+      duration: 2,
     }
   );
+};
+const loadingEnter = () => {
+  let timeline = gsap.timeline();
+
+  timeline.fromTo(
+    "loading-bg",
+    {
+      y: 0,
+    },
+    {
+      y: "100%",
+      duration: 2,
+    }
+  );
+};
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      name: "page-wipe",
+      async leave(data) {
+        const done = this.async();
+        loadingLeave();
+        await delay(1500);
+        done();
+      },
+      async enter(data) {
+        loadingEnter();
+        //initPage();
+      },
+      async once(data) {
+        initPage();
+      },
+    },
+  ],
+});
--- gsap/js/dist/app.dev.js
+++ gsap/js/dist/app.dev.js
@@ -1,86 +1,172 @@
 "use strict";
 
-var timeline = gsap.timeline();
-timeline.fromTo(".info-section h1", {
-  x: 0,
-  y: 0,
-  opacity: 0
-}, {
-  x: 0,
-  y: 0,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1
-}).fromTo(".info-section h4", {
-  x: -200,
-  opacity: 0
-}, {
-  x: 0,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1
-}).fromTo(".logo", {
-  x: -200,
-  opacity: 0
-}, {
-  x: 0,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1.5
-}).fromTo(".menu", {
-  x: 200,
-  opacity: 0
-}, {
-  x: 0,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1.5
-}, ">-1.8").fromTo(".girl", {
-  x: -500,
-  opacity: 0
-}, {
-  x: 0,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1.3
-}, ">-1.8").fromTo(".boy", {
-  x: 0,
-  opacity: 0
-}, {
-  x: 275,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1.3
-}, ">-1.8").fromTo(".shape1", {
-  scale: 0.1,
-  opacity: 0
-}, {
-  scale: 1,
-  opacity: 1,
-  delay: 0.5,
-  duration: 1.3
-}, ">-1.8").fromTo(".shape2", {
-  scale: 0.1,
-  opacity: 0
-}, {
-  scale: 1,
-  opacity: 0.4,
-  delay: 0.5,
-  duration: 1.3
-}, ">-1.8").fromTo(".shape3", {
-  scale: 0,
-  opacity: 0
-}, {
-  scale: 1,
-  opacity: 0.4,
-  delay: 0.5,
-  duration: 1.3
-}, ">-1.8").fromTo(".call-actions", {
-  scale: 0,
-  opacity: 0
-}, {
-  scale: 1,
-  opacity: 1,
-  //delay: 0.5,
-  duration: 1
+var initPage = function initPage() {
+  var timeline = gsap.timeline();
+  timeline.fromTo(".info-section h1", {
+    x: 0,
+    y: 0,
+    opacity: 0
+  }, {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1
+  }).fromTo(".info-section h4", {
+    x: -200,
+    opacity: 0
+  }, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1
+  }).fromTo(".logo", {
+    x: -200,
+    opacity: 0
+  }, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1.5
+  }).fromTo(".menu", {
+    x: 200,
+    opacity: 0
+  }, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1.5
+  }, ">-1.8").fromTo(".back", {
+    x: -500,
+    opacity: 0
+  }, {
+    x: 0,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1.3
+  }, ">-1.8").fromTo(".front", {
+    x: 0,
+    opacity: 0
+  }, {
+    x: 275,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1.3
+  }, ">-1.8").fromTo(".shape1", {
+    scale: 0.1,
+    opacity: 0
+  }, {
+    scale: 1,
+    opacity: 1,
+    delay: 0.5,
+    duration: 1.3
+  }, ">-1.8").fromTo(".shape2", {
+    scale: 0.1,
+    opacity: 0
+  }, {
+    scale: 1,
+    opacity: 0.4,
+    delay: 0.5,
+    duration: 1.3
+  }, ">-1.8").fromTo(".shape3", {
+    scale: 0,
+    opacity: 0
+  }, {
+    scale: 1,
+    opacity: 0.4,
+    delay: 0.5,
+    duration: 1.3
+  }, ">-1.8").fromTo(".call-actions", {
+    scale: 0,
+    opacity: 0
+  }, {
+    scale: 1,
+    opacity: 1,
+    //delay: 0.5,
+    duration: 1
+  });
+};
+
+var delay = function delay(n) {
+  return new Promise(function (done) {
+    setTimeout(function () {
+      done();
+    });
+  });
+};
+
+var loadingLeave = function loadingLeave() {
+  var timeline = gsap.timeline();
+  timeline.fromTo("loading-bg", {
+    y: "100%"
+  }, {
+    y: 0,
+    duration: 2
+  });
+};
+
+var loadingEnter = function loadingEnter() {
+  var timeline = gsap.timeline();
+  timeline.fromTo("loading-bg", {
+    y: 0
+  }, {
+    y: "100%",
+    duration: 2
+  });
+};
+
+barba.init({
+  sync: true,
+  transitions: [{
+    name: "page-wipe",
+    leave: function leave(data) {
+      var done;
+      return regeneratorRuntime.async(function leave$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              done = this.async();
+              loadingLeave();
+              _context.next = 4;
+              return regeneratorRuntime.awrap(delay(1500));
+
+            case 4:
+              done();
+
+            case 5:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, null, this);
+    },
+    enter: function enter(data) {
+      return regeneratorRuntime.async(function enter$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              loadingEnter(); //initPage();
+
+            case 1:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      });
+    },
+    once: function once(data) {
+      return regeneratorRuntime.async(function once$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+              initPage();
+
+            case 1:
+            case "end":
+              return _context3.stop();
+          }
+        }
+      });
+    }
+  }]
 });
